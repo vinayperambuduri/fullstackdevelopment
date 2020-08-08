ba43c3fabd044e6b6927c38d431f344ada9ce1e4
unknown <vinayperambuduri@gmail.com>
2020-08-08 21:40:11 +0530

    http site changed to https

--- module5-webapp/js/script.js
+++ module5-webapp/js/script.js
@@ -14,10 +14,10 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     var dc = {};
 
     var homeHtmlUrl = "snippets/home-snippet.html";
-    var allCategoriesUrl = "http://davids-restaurant.herokuapp.com/categories.json";
+    var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
-    var menuItemsUrl = "http://davids-restaurant.herokuapp.com/menu_items.json?category=";
+    var menuItemsUrl = "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
     var menuItemsTitleHtml = "snippets/menu-items-title.html";
     var menuItemHtml = "snippets/menu-item.html";
 
